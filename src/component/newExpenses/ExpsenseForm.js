18f4d04ce0ec080939ad03729271ff1b5deab547
shital <shitalunde25@gmail.com>
2022-09-12 00:35:15 +0530

    4th session completed

--- src/component/newExpenses/ExpsenseForm.js
+++ src/component/newExpenses/ExpsenseForm.js
@@ -1,28 +1,111 @@
-import './ExpenseForm.css'
-
+import "./ExpenseForm.css";
+import { useState } from "react";
 const ExpenseForm = (props) => {
+  // const [enteredTitle, setEnteredTitle] = useState('')
+  // const [enteredAmount, setEnteredAmount] = useState('')
+  // const [enteredDate, setEnteredDate] = useState('')
 
- return (
-    <form>
-        <div className='new-expense__controls'>
-            <div className='new-expense__control'>
-                <label>TItle</label>
-                <input type="text"/>
-            </div>
-            <div className='new-expense__control'>
-                <label>Number</label>
-                <input type="number"/>
-            </div>
-            <div className='new-expense__control'>
-                <label>Date</label>
-                <input type="date"/>
-            </div>            
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+
+  const titleChangeHandler = (event) => {
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    setUserInput((preState) => {
+        return {
+            ...preState,
+            enteredTitle: event.target.value,
+        }
+    })
+  };
+
+  const AmountChangeHandler = (event) => {
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+
+    setUserInput((preState) => {
+        return {
+            ...preState,
+            enteredAmount: event.target.value,
+        }
+    })
+  };
+
+  const DateChangeHandler = (event) => {
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+
+    setUserInput((preState) => {
+        return {
+            ...preState,
+            enteredDate: event.target.value,
+            // enteredDate:  new Date(event.target.value),
+        }
+    })
+  };
+
+
+  const submitHandler = (event) => {
+        event.preventDefault()
+
+        userInput.enteredDate =  new Date(userInput.enteredDate)
+
+        const expenseData = {
+            id: Math.random().toString(),
+             ...userInput,
+            // title : userInput.enteredTitle,
+            // amount: userInput.enteredAmount,
+           // date: new Date(userInput.enteredDate)
+        }
+
+        props.onSaveExpenseData(expenseData);
+
+        // console.log(expenseData)
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>TItle</label>
+          <input
+            type="text"
+            value={userInput.enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
-        <div className='new-expense__actions'>
-            <button type='submit'> Add Expense</button>
+        <div className="new-expense__control">
+          <label>Number</label>
+          <input
+            type="number"
+            value={userInput.enteredAmount}
+            onChange={AmountChangeHandler}
+          />
         </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            value={userInput.enteredDate}
+            onChange={DateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit"> Add Expense</button>
+      </div>
     </form>
- )
-}
+  );
+};
 
-export default ExpenseForm
+export default ExpenseForm;
--- src/component/newExpenses/newExpenses.js
+++ src/component/newExpenses/newExpenses.js
@@ -1,10 +1,15 @@
 import ExpenseForm from "./ExpsenseForm";
 import './newExpenses.css'
 
-const NewExpenses = () => {
+const NewExpenses = (props) => {
+    const saveExpenseDataHandler = (enretedExpenseData) => {
+            console.log('In new Expense ',enretedExpenseData)
+
+          props.onAddExpense(enretedExpenseData)
+    }
  return (
     <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler}/>
     </div>
  )
 }
